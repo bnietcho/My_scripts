f90ba905d3e3597b8c7c278275636351ca00e024
utrains-warriors <utrains12@gmail.com>
2022-10-24 21:46:49 -0500

    script02

---
+++ script1.sh
@@ -0,0 +1,15 @@
+#1/bin/bash
+
+###Author is Blaise Nietcho 
+##Date - 2022-10-24
+
+echo "my name is"
+
+read name 
+
+echo "my profession is"
+
+read P
+
+echo "my name is $name and my profession is $P"
+
